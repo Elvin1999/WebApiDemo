3cb95b6d2624e9a8d8f7dbf5aa41afa21d61bb4d
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-10-13 21:28:40 +0400

    Add project files.

---
+++ WebApiDemo/DataAccess/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Entities;
+
+namespace WebApiDemo.DataAccess
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
